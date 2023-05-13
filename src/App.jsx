0329b0eb3f8b82b8fb57567aeaa860881850d19b
atakriti <anwart256@gmail.com>
2023-05-13 10:43:12 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,18 @@ import "./style.css"
 import QRCode from 'qrcode.react';
 function App() {
   const [url,setUrl] = useState("")
+  const [title,setTitle] = useState("")
  
   
   return (
     <div className="App">
       <h1>QR Generator</h1>
+     <input type="text" name="title" placeholder='Enter Title...' onChange={(e) => setTitle(e.target.value)} />
       {!url &&  <input type="text" name="url" placeholder='Enter your URL...' onChange={(e) => setUrl(e.target.value)} />}
       {url &&  <button onClick={() => setUrl("")}>Again</button>}
       
-      {url && <a href={url}>Your URL</a>} 
+      {url && <a target='_blank' href={url}>Your URL</a>} 
+      {title && <p>{title}</p>} 
       {url && <QRCode value={url} />}
     </div>
   );
